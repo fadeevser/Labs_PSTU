56324b043815a4e926b23502873f520ad073124a
fadeevser <73518236+fadeevser@users.noreply.github.com>
2021-04-11 09:37:39 +0500

    Add files via upload

---
+++ Lab_6/Lab_6.cpp
@@ -0,0 +1,23 @@
+#include <iostream> //done
+using namespace std;
+int a;
+int f(int n)
+{
+    if (n / 10 > 0)
+    {
+        return n % 10 + f(n / 10);
+    }
+    else
+    {
+        return n % 10;
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    cout << "Введите число для поиска его элементов: ";
+    cout << endl;
+    cin >> a;
+    cout << f(a);
+    return 0;
+}
